5839c3969fa6f99a5bd893020c8f7d51ad67ad62
sujesh <sujeshmanshakya@gmail.com>
2020-12-27 10:16:03 +1300

    Lifting up state

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,51 +2,14 @@ import React, { Component } from 'react';
 import Counter from './counter';
 
 class Counters extends Component {
-    state = {
-        counters: [
-        {id:1, value:4},
-        {id:2, value:0},
-        {id:3, value:0},
-        {id:4, value:0}
-    ]  };
-
-    handleDelete= (counterId) =>{
-        const counters = this.state.counters.filter(c => c.id !== counterId);
-        this.setState({counters});
-    }
-
-    handleIncrement= (counter) =>{
-        const counters = [...this.state.counters];
-        const index = counters.indexOf(counter);
-        counters[index] = {...counter};
-        counters[index].value++;
-        this.setState({counters});
-    }
-
-    handleDecrement= (counter) =>{
-        const counters = [...this.state.counters];
-        const index = counters.indexOf(counter);
-        counters[index] = {...counter};
-        counters[index].value--;
-        this.setState({counters});
-    }
-
-    onReset = () =>{
-        const counters = this.state.counters.map(x=> {
-            x.value = 0
-            return x;
-        })
-        this.setState({counters});
-    }
- 
     render() { 
         return (<div>
-            <button className="btn-primary m-2" onClick={this.onReset}>Reset</button>
+            <button className="btn-primary m-2" onClick={this.props.onReset}>Reset</button>
             {
-            this.state.counters.map(counter => <Counter key={counter.id} counter={counter} 
-            onIncrement={this.handleIncrement}
-            onDecrement={this.handleDecrement}
-            onDelete={this.handleDelete}>
+            this.props.counters.map(counter => <Counter key={counter.id} counter={counter} 
+            onIncrement={this.props.onIncrement}
+            onDecrement={this.props.onDecrement}
+            onDelete={this.props.onDelete}>
                 <h4>Counter: {counter.id}</h4>
             </Counter>)
             }</div> );
